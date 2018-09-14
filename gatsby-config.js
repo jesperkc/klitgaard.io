4ea7e6086fb6118058f1334ba09bc87308677a2e
Jesper Strange Klitgaard Christiansen <jesperkc@gmail.com>
2018-09-14 10:43:27 +0200

    Move static

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Jesper Strange Klitgaard Christiansen',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
